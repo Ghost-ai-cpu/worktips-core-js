623f9d6c5f649ecdf847245e07143e5a4c0d74e7
Ghost-ai-cpu <72390639+Ghost-ai-cpu@users.noreply.github.com>
2022-12-04 14:46:18 +0530

    Files added

---
+++ bin/archive-emcpp.sh
@@ -0,0 +1,7 @@
+#!/bin/sh
+
+bin/build-emcpp.sh &&
+cp build/WorktipsCoreCpp_WASM.js worktips_utils/; 
+cp build/WorktipsCoreCpp_WASM.wasm worktips_utils/;
+cp build/WorktipsCoreCpp_ASMJS.js worktips_utils/; 
+cp build/WorktipsCoreCpp_ASMJS.asm.js worktips_utils/ 
